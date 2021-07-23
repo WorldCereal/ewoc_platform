5c763d997c4111f8626ea8790a3ad45367f62b3e
Nicolas Lacroux <nicolas.lacroux.ext@csgroup.eu>
2021-07-23 13:44:48 +0200

    Upadate env variables

--- export-env.sh
+++ export-env.sh
@@ -3,14 +3,14 @@
 
 # set env variable for Prod cluster
 if [ "$USER" == "ewoc-prod" ]; then
-    export CLUSTER="ewoc-prod"
+    export TARGET_CLUSTER="ewoc-prod"
     export HOSTNAME="prod.esa-worldcereal.org"
     export FLOATING_IP="185.178.84.50"
     echo "test"
 
 # set env variable for Test cluster
 elif [ "$USER" == "ewoc-test" ]; then
-    export CLUSTER="ewoc-test"
+    export TARGET_CLUSTER="ewoc-test"
     export HOSTNAME="test.esa-worldcereal.org"
     export FLOATING_IP="xxx.xxx.xxx.xxx"
 
@@ -26,5 +26,3 @@ export KONG_VERSION="2.4"
 # Keycloak conf
 export KEYCLOAK_CHART_VERSION="2.4.7"
 export KEYCLOAK_VERSION="12.0.4-debian-10-r69" 
-export KEYCLOAK_HOSTNAME="auth."$HOSTNAME
-
