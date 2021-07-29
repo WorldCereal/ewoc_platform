089469f455099fde6ed98afa55ce93aa6d94b012
Nicolas Lacroux <nicolas.lacroux.ext@csgroup.eu>
2021-07-29 16:40:27 +0200

    Fix hostname value

--- kong-routes-services.sh
+++ kong-routes-services.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-local HOSTNAME="auth.185.178.85.22"
 
 ### Add keycloak Service 
 curl -kSsl -X POST \
 --url https://localhost:8444/services/ \
 --data 'name=keycloak' \
---data 'url=http://'$HOSTNAME':80'
+--data 'url=http://185.178.85.22:80/auth"'
 
 ### Add keycloak route
 
 curl -kSsl -X POST \
 --url https://localhost:8444/services/keycloak/routes \
---data 'hosts[]='$HOSTNAME \
---data 'paths[]=/' \
+--data 'paths[]=/auth"' \
 --data 'strip_path=false' \
 
 touch toto.test
