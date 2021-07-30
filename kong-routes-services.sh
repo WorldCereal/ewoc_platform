3b02422f54704cd51f1de9d3973d77175a2721f6
Nicolas Lacroux <nicolas.lacroux.ext@csgroup.eu>
2021-07-30 14:58:33 +0200

    Fix kong script

--- kong-routes-services.sh
+++ kong-routes-services.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 
-### Add keycloak Service 
+### Add Keycloak Service  #### keycloak.local
 curl -kSsl -X POST \
 --url https://localhost:8444/services/ \
 --data 'name=keycloak' \
---data 'url=http://185.178.85.22:80/auth"'
+--data 'url=http://keycloak.local:80'
 
-### Add keycloak route
+### Add Keycloak route ####
 
 curl -kSsl -X POST \
 --url https://localhost:8444/services/keycloak/routes \
---data 'paths[]=/auth"' \
+--data 'hosts[]=185.178.85.22'
+--data 'paths[]=/auth' \
 --data 'strip_path=false' \
+
+
+### Add Grafana Service ###
+
+### Add Grafana routes ###
+
+### Add Grafana Service ###
+
+### Add Grafana routes ###
+
