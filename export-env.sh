2de053b9924ce3da6d4911061531fff02be0cfc5
Nicolas Lacroux <nicolas.lacroux.ext@csgroup.eu>
2021-08-11 14:37:45 +0200

    [REFACTOR] archive Safescale work

--- export-env.sh
+++ export-env.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
 
-# set env variable for Prod cluster
-if [ "$USER" == "ewoc-prod" ]; then
-    export TARGET_CLUSTER="ewoc-prod"
-    #export HOSTNAME="prod.esa-worldcereal.org"
-    export HOSTNAME="185.178.85.22"
-    export FLOATING_IP="185.178.85.22"
-    echo "test"
-
-# set env variable for Test cluster
-elif [ "$USER" == "ewoc-test" ]; then
-    export TARGET_CLUSTER="ewoc-test"
-    export HOSTNAME="test.esa-worldcereal.org"
-    export FLOATING_IP="xxx.xxx.xxx.xxx"
-
-else 
-    echo "USER env variable is missing, it's mandatory, you must run the script with the safescale ewoc user: prod or test"
-    exit 0 
-fi
-
+export TARGET_CLUSTER="ewoc-prod"
+#export HOSTNAME="prod.esa-worldcereal.org"
+export HOSTNAME="45.130.29.65"
+export FLOATING_IP="45.130.29.65"
 
 # Kong conf
 export KONG_CHART_VERSION="2.1.0"
-export KONG_VERSION="2.4"
+export KONG_VERSION="2.4-ubuntu"
+
+export POSTGRESQL_CHART_VERSION="10.3.15"
 
 # Keycloak conf
 export KEYCLOAK_CHART_VERSION="2.4.7"
